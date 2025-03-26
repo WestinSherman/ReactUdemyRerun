0132a63281bb7e78d4caffa832555becc9152e9c
WestinSherman <westin@arnst.ca>
2025-03-26 15:47:45 +0100

    S_66(642), finished.

--- First-React-App/src/components/Counter.jsx
+++ First-React-App/src/components/Counter.jsx
@@ -1,13 +1,14 @@
+import { useState } from 'react';
+
 export default function Counter() {
-    let num = 0;
-    const incrementNum = () => {
-        num += 1;
-        console.log(num);
-    };
+    const [num, setNum] = useState(0);
+    const changeNum = () => {
+        setNum(num + 1);
+    }
     return (
         <div>
             <p>The count is: {num}</p>
-            <button onClick={incrementNum}>Increment</button>
+            <button onClick={changeNum}>Increment</button>
         </div>
     )
 }
